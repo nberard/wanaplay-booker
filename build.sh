5ba5713105e10e2ec1b93859459542a298dfd4a6
Nicolas Berard <nicolas.berard@canaltp.fr>
2019-04-18 10:40:13 +0200

    add push

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 docker pull clux/muslrust
 docker run -v $PWD:/volume -v cargo-cache:/root/.cargo/registry --rm -t clux/muslrust cargo build --release
 docker build -t touplitoui/wanaplay-booker-bot .
+docker push touplitoui/wanaplay-booker-bot
